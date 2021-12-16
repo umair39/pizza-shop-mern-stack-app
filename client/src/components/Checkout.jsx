382bcacb1a7d39747f56860664ae075e4176cc58
Muhammad Umair <56631158+umair39@users.noreply.github.com>
2021-12-16 17:35:57 +0500

    Update Checkout.jsx

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -24,7 +24,7 @@ const Checkout = ({ subTotal }) => {
         amount={subTotal * 100}
         shippingAddress
         token={tokenHandler}
-        stripeKey="insert stripe key here"
+        stripeKey="pk_test_51Jw49vAq9NC7TbSBPA6K8gXzauiJ7C7kGgATKF0MiEoW8gw7qQQjYVdSCaxZvEVX92TJ0vTrRHEZ3VnRs3NgdDKP00DbM0SZlT"
         currency="PKR"
       >
         <Button>Pay Now</Button>
